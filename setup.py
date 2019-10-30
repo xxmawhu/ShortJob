266fc0b2d61dd7cba1be9c31308ba42e3b094387
xxmawhu <xxmawhu@163.com>
2019-10-30 17:03:32 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : Xin-Xin MA
+#   Email         : xxmawhu@163.com
+#   File Name     : setup.py
+#   Created Time  : 2019-09-24 10:51
+#   Last Modified : 2019-09-29 12:09
+#   Describe      :
+#
+# ====================================================
+
+from setuptools import setup
+from setuptools import find_packages
+import core
+import subprocess
+import sys
+import os
+setup(
+    name='ShortJob',
+    version='1.0',
+    author='Xin-Xin Ma',
+    packages=find_packages(),
+    project_urls={
+    'Source': 'https://github.com/xxmawhu/ShortJob',
+	}
+)
