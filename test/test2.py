9f93714fbacc1c22f62f93eae2e374b092ed3529
xxmawhu <xxmawhu@163.com>
2020-03-14 15:55:27 +0800

    minor

--- test/test2.py
+++ test/test2.py
@@ -2,17 +2,6 @@
 import os
 from ShortJob import MkMulJob
 
-# get all files in the current directory
-fileList = os.listdir(".")
-
-# only need those file end with .C
-scriptList = []
-for f in fileList:
-    if f.endswith(".C"):
-        scriptList.append(f)
-
 mk = MkMulJob.MkMulJob(["run.C", "test.py", "run.sh"])
-for i, f in enumerate(scriptList):
-    input_value = [f[:-2]]
-    print input_value
+for i  in range(10):
     mk.Make(i, [(3.14, 2.3, "a"), ("a", "b", "c"), ("a")])
